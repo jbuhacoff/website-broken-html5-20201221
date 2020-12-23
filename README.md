ce0f726f0613f50bf6e0a2db1180dd5dacf2b053
Jonathan Buhacoff <jonathan@buhacoff.net>
2020-12-23 11:53:11 -0800

    fix: production build works with gridsome v0.7.22

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 website-broken-html5-20201221
 =============================
 
-Development server works:
+Development server works with gridsome v0.7.22 and v0.7.23:
 
 ```
 npm run develop
 ```
 
-Production build is broken:
+Production build works with gridsome v0.7.22, but is broken gridsome v0.7.23:
 
 ```
 npm run build
--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,5 @@
 module.exports = {
     presets: [
-        ['@vue/babel-preset-app', {
-            targets: ['> 5%'],
-        }],
-    ],
-    // this doesn't seem to have any effect:
-    plugins: [
-        ['@babel/plugin-transform-runtime', {
-            'corejs': 3,
-            "helpers": true,
-            "regenerator": true,
-            "useESModules": false
-        }],
+        '@vue/babel-preset-app',
     ],
 };
